4afc01541b3a5830cc11c3aed2440bcd33069bf2
周路尧 <zhouluyao@meicai.cn>
2016-01-11 16:52:36 +0800

    Initial Commit

---
+++ LYDrawBoarder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LYDrawBoarder
+//
+//  Created by lychow on 1/11/16.
+//  Copyright © 2016 IOSDeveloper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
